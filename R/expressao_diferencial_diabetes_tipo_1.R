fd2869b4e7c8c6929ae51d2907c2f31ebb719cc2
luxeredias <luxeredias@gmail.com>
2022-07-13 15:22:23 -0300

    todos na mesma página

--- R/expressao_diferencial_diabetes_tipo_1.R
+++ R/expressao_diferencial_diabetes_tipo_1.R
@@ -14,14 +14,65 @@ setwd("/Users/Usuario/Desktop/Curso ciências ômicas/")
 count_data <- read.csv(file = "GSE162689/GSE162689_raw_counts_all.txt.gz",sep = "\t")
 count_data <- count_data[-1,]
 
-accession2id <- read.csv("https://raw.githubusercontent.com/csbl-inovausp/RNAseq_DESeq2_R_tutorial/main/accession2id.csv")
+count_data <- count_data %>%
+  filter(!is.na(is_133_001),
+         !grepl(pattern = "-Mar|-Sep|-Dec",Title))
 
-sra_run_table <- read.csv("https://raw.githubusercontent.com/csbl-inovausp/RNAseq_DESeq2_R_tutorial/main/SraRunTable.txt")
+#Carregar metadados e manipular as tabelas
+accession2id <- read.csv("https://raw.githubusercontent.com/csbl-inovausp/RNAseq_DESeq2_R_tutorial/main/data/accession2id.csv")
+
+sra_run_table <- read.csv("https://raw.githubusercontent.com/csbl-inovausp/RNAseq_DESeq2_R_tutorial/main/data/SraRunTable.txt")
 
 proto_pheno <- sra_run_table %>%
   left_join(y = accession2id,by = c("Sample.Name"="GEO_Accession"))
 
 pheno_data <- proto_pheno %>%
-  dplyr::select(sample_id,disease_state)
-  
-  
+  dplyr::select(sample_id,disease_state,tissue)
+
+pheno_data$disease_state <- factor(x = pheno_data$disease_state,
+                                   levels = c("non-diabetic","type 1 diabetic"))
+
+pheno_islet <- pheno_data %>%
+  filter(tissue=="Islet tissue")
+
+islet_cols <- c(1,which(colnames(count_data) %in% pheno_islet$sample_id))
+count_data_islet <- count_data[,islet_cols]
+
+#Criar objeto dds (DESeq2)
+dds <- DESeq2::DESeqDataSetFromMatrix(countData = count_data_islet,
+                                      colData = pheno_islet,
+                                      tidy = T,
+                                      design = ~disease_state)
+
+nrow(dds)
+
+keep_10 <- rowSums(DESeq2::counts(dds) >= 10) >= 5
+dds_filtrado_final <- dds[keep_10,]
+
+nrow(dds_filtrado_final)
+
+#Plotar PCA das amostras
+vsd <- DESeq2::vst(dds_filtrado_final, blind = FALSE)
+DESeq2::plotPCA(vsd, intgroup = "disease_state")  
+
+
+#Plotar heatmap de distância das amostras
+sampleDists <- dist(t(assay(vsd)))
+
+library("pheatmap")
+library("RColorBrewer")
+
+sampleDistMatrix <- as.matrix( sampleDists )
+rownames(sampleDistMatrix) <- paste( vsd$disease_state, sep = " - " )
+colnames(sampleDistMatrix) <- paste( vsd$disease_state, sep = " - " )
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows = sampleDists,
+         clustering_distance_cols = sampleDists,
+         col = colors)
+
+#Rodar análise de expressão diferencial
+
+
+
+
